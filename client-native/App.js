0fbfb8d19c7027f86b5a874d8ee5da589fb19bd7
Te-Yen Wu <te-yenwu@MacBook-Pro-3.local>
2023-11-21 16:47:49 -0500

    mend

--- client-native/App.js
+++ client-native/App.js
@@ -4,7 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#111' }}>Open up App.js to start fcking on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
